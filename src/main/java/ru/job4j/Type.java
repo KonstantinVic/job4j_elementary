9157872280610628df51a221e1274953742b7af3
konstantin <konstantinahkalov@gmail.com>
2021-10-24 15:26:53 +0300

    2. Переменные. [#172635]

---
+++ src/main/java/ru/job4j/Type.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Type {
+    public static void main(String[] args) {
+        byte age = 38;
+        System.out.println(age);
+        char sex = 'M';
+        System.out.println(sex);
+        float height = 1.88F;
+        System.out.println(height);
+        short brick = 28458;
+        System.out.println(brick);
+        int bacterium = 10000000;
+        System.out.println(bacterium);
+        double distance = 152000000.00;
+        System.out.println(distance);
+        boolean work = true;
+        System.out.println(work);
+        byte mem = 1;
+        System.out.println(mem);
+    }
+}
