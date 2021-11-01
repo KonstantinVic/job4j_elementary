65a2f3b813da6029c6721970a1891016c3a27dae
Kostya <konstantinuhkalov@gmail.com>
2021-11-01 11:52:37 +0200

    Косяк

---
+++ src/main/java/ru/job4j/math/MathMath.java
@@ -0,0 +1,11 @@
+package ru.job4j.math;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathMath {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+}
